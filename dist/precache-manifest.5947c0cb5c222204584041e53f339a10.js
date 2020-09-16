54094e6195ea47f2057df8518b0a568d2cf397ca
liruifengv <li15227870916@gmail.com>
2020-09-16 13:43:32 +0800

    pws

---
+++ dist/precache-manifest.5947c0cb5c222204584041e53f339a10.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e16b1bd15cd2327ccdb",
+    "url": "/css/app.601d2ada.css"
+  },
+  {
+    "revision": "a76d3d2e89d60c571efb2dd3a4745ec7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe9a9135c23ed231d05f",
+    "url": "/js/about.3ce83438.js"
+  },
+  {
+    "revision": "4e16b1bd15cd2327ccdb",
+    "url": "/js/app.3bc8bebe.js"
+  },
+  {
+    "revision": "13f30aa69c5aa5616414",
+    "url": "/js/chunk-vendors.6c90c66f.js"
+  },
+  {
+    "revision": "38b72014e187c5f4acd036ebc1c9eed6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "ca21bb38c115ae0d6c4e8cda03b8d742",
+    "url": "/sw.js"
+  }
+]);
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
-const path = require('path')
 
+/* eslint-disable */
+const path = require('path')
+const manifestJSON = require('./public/manifest.json')
 function resolve (dir) {
   return path.join(__dirname, dir)
 }
@@ -13,19 +15,10 @@ module.exports = {
     }
   },
   pwa: {
-    name: 'My App',
-    themeColor: '#4DBA87',
-    msTileColor: '#000000',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
-
-    // 配置 workbox 插件
-    workboxPluginMode: 'InjectManifest',
+    themeColor: manifestJSON.theme_color,
     workboxOptions: {
-      // InjectManifest 模式下 swSrc 是必填的。
-      swSrc: 'service-worker.js',
-      runtimeCaching: 'CacheOnly'
-      // ...其它 Workbox 选项...
+      skipWaiting: true,
+      clientsClaim: true
     }
   }
 }
